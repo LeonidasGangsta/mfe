9d72748cc5c955826d9551ce43cfdb1b1e0d112a
Oscar Torres <oscardtorres@hotmail.com>
2022-08-21 20:20:34 -0500

    Provided a prefix for Material UI and solve overlapping class names

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
 import Header from './coponents/Header';
 import MarketingApp from './coponents/MarketingApp';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
+
 export default () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <hr />
-      <Route path="/" exact component={MarketingApp} />
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header />
+        <hr />
+        <Route path="/" exact component={MarketingApp} />
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
